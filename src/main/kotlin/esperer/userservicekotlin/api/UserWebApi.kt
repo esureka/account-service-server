660c919efa48fb5759d2cabac0dbfdaadd183e82
aesperer <piyrw9754@gmail.com>
2023-07-24 15:12:18 +0900

    user api

--- src/main/kotlin/esperer/userservicekotlin/api/UserWebApi.kt
+++ src/main/kotlin/esperer/userservicekotlin/api/UserWebApi.kt
@@ -1,15 +1,24 @@
 package esperer.userservicekotlin.api
 
+import esperer.userservicekotlin.service.UserService
 import esperer.userservicekotlin.vo.Greeting
+import esperer.userservicekotlin.vo.RequestUser
+import org.modelmapper.ModelMapper
+import org.modelmapper.convention.MatchingStrategies
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/")
 class UserWebApi(
-    private val greeting: Greeting
+    private val greeting: Greeting,
+    private val userService: UserService
 ) {
 
-    @GetMapping("/")
+    @GetMapping("/health-check")
     fun healthCheck(): String {
         return "It's working in user service"
     }
@@ -17,5 +26,11 @@ class UserWebApi(
     @GetMapping("/welcome")
     fun welcome(): String {
         return greeting.message
+    }
+
+    @PostMapping("/users")
+    fun createUser(@RequestBody requestUser: RequestUser): String {
+        userService.createUser(requestUser)
+        return "Create user method is called"
     }
 }
