45c7cf143303a2c15cf0bd711d1bad662ff753e3
aesperer <piyrw9754@gmail.com>
2023-07-24 13:27:25 +0900

    user service

---
+++ src/test/kotlin/esperer/userservicekotlin/UserservicekotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package esperer.userservicekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserservicekotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
