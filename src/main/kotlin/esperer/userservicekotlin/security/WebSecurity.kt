719022ac884d3880c0b4cd8fe35d073dfbbfb4f5
aesperer <piyrw9754@gmail.com>
2023-08-01 18:58:46 +0900

    create authentication manager

--- src/main/kotlin/esperer/userservicekotlin/security/WebSecurity.kt
+++ src/main/kotlin/esperer/userservicekotlin/security/WebSecurity.kt
@@ -3,19 +3,30 @@ package esperer.userservicekotlin.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class WebSecurity {
+class WebSecurity : WebSecurityConfigurerAdapter() {
 
     @Bean
     protected fun configure(http: HttpSecurity): SecurityFilterChain {
         http.csrf().disable()
-        http.authorizeRequests().antMatchers("/users/**").permitAll()
+        http.authorizeRequests().antMatchers("/users/**")
+            .hasIpAddress("192.168.0.8")
+            .and()
+            .addFilter(getAuthenticationFilter())
         http.headers().frameOptions().disable()
 
         return http.build()
+    }
+
+    private fun getAuthenticationFilter(): AuthenticationFilter {
+        val authenticationFilter = AuthenticationFilter()
+        authenticationFilter.setAuthenticationManager(authenticationManager())
+
+        return authenticationFilter
     }
 
     @Bean
