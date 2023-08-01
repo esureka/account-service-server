6cc897c357c5557b25888257ab171cb677e0f023
aesperer <piyrw9754@gmail.com>
2023-08-01 18:59:44 +0900

    configure return bulild delete

--- src/main/kotlin/esperer/userservicekotlin/security/WebSecurity.kt
+++ src/main/kotlin/esperer/userservicekotlin/security/WebSecurity.kt
@@ -5,21 +5,18 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class WebSecurity : WebSecurityConfigurerAdapter() {
 
     @Bean
-    override fun configure(http: HttpSecurity): SecurityFilterChain {
+    override fun configure(http: HttpSecurity) {
         http.csrf().disable()
         http.authorizeRequests().antMatchers("/users/**")
             .hasIpAddress("192.168.0.8")
             .and()
             .addFilter(getAuthenticationFilter())
         http.headers().frameOptions().disable()
-
-        return http.build()
     }
 
     private fun getAuthenticationFilter(): AuthenticationFilter {
