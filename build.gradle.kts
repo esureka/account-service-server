ce5f16bfca68cd4850fc08b7de200e3e99306b65
aesperer <piyrw9754@gmail.com>
2023-08-10 13:22:38 +0900

    add :: amqp depend;

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,9 +36,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-
-
-
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
 
     runtimeOnly("com.mysql:mysql-connector-j")
 
